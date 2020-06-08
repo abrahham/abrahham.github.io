4f77b3c9d0c8e0086c2d727ba0b4b8ffc6b36bf6
= <valvdomero@gmail.com>
2020-06-08 00:02:20 -0500

    Modificando barra nav de paneles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var botones = document.querySelectorAll('#dash button');
 var paneles = document.querySelectorAll('.panel');
 botones.forEach((boton, idx) => {
-    boton.classList.remove('selected');
     boton.addEventListener('click', () => {
+        botones.forEach(boton => boton.classList.remove('selected'));
         paneles.forEach(panel => panel.style.display = 'none');
         paneles[idx].style.display = 'block';
         boton.classList.add('selected');
